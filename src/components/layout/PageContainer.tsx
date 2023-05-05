50afb3b2466dd40f45fdfb73c9b22cdd3d652aeb
Sergey Aglushevich <s.aglushevich5@gmail.com>
2023-05-05 15:32:27 +0300

    perf: add lazy imports

--- src/components/layout/PageContainer.tsx
+++ src/components/layout/PageContainer.tsx
@@ -1,22 +1,23 @@
-import { ReactNode } from "react";
+import { lazy, ReactNode, Suspense } from "react";
 import { useTranslation } from "react-i18next";
 
-import Footer from "@components/Footer";
 import Navigation from "@components/UI/Navigation";
 
 import { Discount, Text } from "./styles";
+
+const LazyFooter = lazy(() => import("@components/Footer"));
 
 export function PageContainer({ children }: { children: ReactNode }) {
   const { t } = useTranslation();
 
   return (
-    <>
+    <Suspense>
       <Navigation />
       <Discount>
         <Text>{t("discount")}</Text>
       </Discount>
       {children}
-      <Footer />
-    </>
+      <LazyFooter />
+    </Suspense>
   );
 }
