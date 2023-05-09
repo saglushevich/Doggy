9074a7ede32c0d28b4d99fe880e353962f903b07
Sergey Aglushevich <s.aglushevich5@gmail.com>
2023-05-09 21:59:58 +0300

    test: update searchDog test

--- cypress/e2e/searchDog.cy.js
+++ cypress/e2e/searchDog.cy.js
@@ -3,16 +3,6 @@ describe("search dog", () => {
       cy.visit("localhost:3000/info");
     });
 
-    it("not found status", () => {
-        cy.get("input[placeholder='Search']").type("Sibsaxeriban")
-        cy.get("li").contains("Not found! Try again!").should("exist");
-    });
-
-    it("loading status", () => {
-        cy.get("input[placeholder='Search']").type("Siberian")
-        cy.get("li").contains("Loading...").should("exist");
-    });
-
     it("find dog", () => {
         cy.get("input[placeholder='Search']").type("Siberian")
         cy.get("li").contains("Siberian Husky").should("exist");
